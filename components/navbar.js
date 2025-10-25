6959b35ae5a5de6bbab3ee1ea2885dda3fb62221
tmitmac <trentmckenzie09@outlook.com>
2025-10-25 22:11:28 +1300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -84,7 +84,7 @@ class CustomNavbar extends HTMLElement {
         }
       </style>
       <nav>
-        <a href="/" class="logo">
+        <a href="index.html" class="logo">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="24" height="24" rx="4" fill="white"/>
             <rect x="6" y="6" width="12" height="12" rx="2" fill="#1e40af"/>
